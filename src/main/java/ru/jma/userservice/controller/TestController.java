e0c34bce13466dd62a792f546eb8069bfb07635b
Polina Kolzunova <kolzunova.dev@gmail.com>
2021-09-28 19:01:04 +0300

    logging with lombok

--- src/main/java/ru/jma/userservice/controller/TestController.java
+++ src/main/java/ru/jma/userservice/controller/TestController.java
@@ -1,27 +1,25 @@
 package ru.jma.userservice.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/1")
     public String test1() {
-        LOGGER.info("request '/users/test/1' was sent");
+        log.info("request '/users/test/1' was sent");
         return "UserService: test endpoint 1";
     }
 
     @GetMapping("/2")
     public String test2() {
-        LOGGER.info("request '/users/test/2' was sent");
-        LOGGER.warn("request '/users/test/2' is deprecated!");
+        log.info("request '/users/test/2' was sent");
+        log.warn("request '/users/test/2' is deprecated!");
         return "UserService: test endpoint 2";
     }
 }
